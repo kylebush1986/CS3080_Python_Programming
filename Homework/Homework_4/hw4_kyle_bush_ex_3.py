bcadbb939aa9104fc40b8f0aa8c1787446c7be24
Kyle Bush <kylebush1986@gmail.com>
2020-10-12 10:44:05 -0600

    Finished homework 4. Still need to zip and submit

--- Homework/Homework_4/hw4_kyle_bush_ex_3.py
+++ Homework/Homework_4/hw4_kyle_bush_ex_3.py
@@ -12,26 +12,43 @@ import re
 import pyperclip
 
 def main():
-    # Define the regexs for finding phone numbers and emails
-    # TODO: write regex that will find any format of phone number
-    phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
-    # (\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})
-    emailRegex = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+    # Define the regex to find phone numbers
+    phoneNumRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?                # area code
+    (\s|-|\.)?                        # separator
+    (\d{3})                           # first 3 digits
+    (\s|-|\.)                         # separator
+    (\d{4})                           # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
+    )''', re.VERBOSE)
+    
+    # Define the regex to find email addresses
+    emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+      # username
+    @                      # @ symbol
+    [a-zA-Z0-9.-]+         # domain name
+    (\.[a-zA-Z]{2,4})      # dot-something
+    )''', re.VERBOSE)
 
     # Copy the contents of the clipboard
     clipBoardText = pyperclip.paste()
     
-    # Search for phone numbers then again for emails
-    phoneNumberSearchResult = phoneNumRegex.findall(clipBoardText)
-    emailSearchResult = emailRegex.findall(clipBoardText)
-
-    # Combine the results into a comma seperated string
-    allSearchResults = phoneNumberSearchResult + emailSearchResult
-    phoneNumbersAndEmails = ', '.join(allSearchResults)
+    # Search for phone numbers and emails and add them to allMatches
+    allMatches = []
+    for groups in phoneNumRegex.findall(clipBoardText):
+       phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+       if groups[8] != '':
+           phoneNum += ' x' + groups[8]
+       allMatches.append(phoneNum)
+    for groups in emailRegex.findall(clipBoardText):
+       allMatches.append(groups[0])
 
     # Copy search results to the clipboard and alert the user
-    pyperclip.copy(phoneNumbersAndEmails)
-    print('Phone numbers and emails have been copied to the clipboard.')
+    if allMatches:
+        pyperclip.copy('\n'.join(allMatches))
+        print(str(len(allMatches)) + ' matches have been copied to the clipboard.')
+    else:
+        print('No phone numbers or emails found.')
 
 if __name__ == '__main__':
     main()
